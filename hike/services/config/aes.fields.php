50eeed72a4a32c276bc76da7b532cc38f813dbe1
venba <julietepsibai1997@gmail.com>
2020-06-04 16:16:14 +0530

    test

--- hike/services/config/aes.fields.php
+++ hike/services/config/aes.fields.php
@@ -1,8 +1,1 @@
-<?php
-
-define("AES_KEY",	"RNT2018");
-$ENCRYPT_FLDS = array(
-    'Login'=> array('Password')
-);
-
-?>
+er
--- hike/services/config/email.config.php
+++ hike/services/config/email.config.php
@@ -1,9 +1,1 @@
-<?php
-    define('SENDER_EMAIL'	, 'info@hyjiya.com');
-    define('SENDER_PASSWORD', 'JGh5aml5YTIwMTg=');
-    define('EMAIL_HOST'	    , 'smtp.gmail.com');
-    define('EMAIL_PORT'	    , 587);
-    define('SMTP_SECURE'    , 'tls');
-    define('SMTP_AUTH'      , true);
-    define('EMAIL_FROM_NAME', 'RNT');
-?>
+s
